bf5194e664d6869e7c774cbd079b7610780126c2
whinsler@gmail.com <whinsler@gmail.com@42f6019d-fe6e-71f8-cd18-21613ec59585>
2014-11-06 08:43:53 +0000

    Версія без запису балансу в БД (видалити написану логіку для конкретного клієнта)

---
+++ branches/checkcnly/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.ServiceProcess;
+using NETCONLib;
+using NetFwTypeLib;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace BalanceChecker
+{
+	class Program
+	{	
+		/// <summary>
+		/// Главная точка входа для приложения.
+		/// </summary>
+		static void Main()
+		{
+#if (DEBUG)
+			new Service();
+			return;
+#endif
+			ServiceBase[] ServicesToRun;
+			ServicesToRun = new ServiceBase[] 
+			{ 
+				new Service()
+			};
+			ServiceBase.Run(ServicesToRun);
+
+		}
+	}
+}
